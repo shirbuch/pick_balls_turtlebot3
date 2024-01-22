dcf35786c488b79b374bf6848a96e5d538ecbd5b
shirbu <shirbuchner@yahoo.com>
2024-01-22 23:13:20 +0100

    Implement navigation service

--- scripts/navigate.py
+++ scripts/navigate.py
@@ -3,11 +3,35 @@
 from __future__ import print_function
 
 import rospy
+import actionlib
 from pick_balls_turtlebot3.srv import Navigate,NavigateResponse
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+def movebase_client(x, y, w=1.0):
+    # moves the robot collision free to a x,y,theta pose (must be valid/reachable in the map)
+    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+    client.wait_for_server()
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.position.z = 0.0
+    goal.target_pose.pose.orientation.w = w
+    print("goal sent:")
+    print(goal)
+    client.send_goal(goal)
+    wait = client.wait_for_result()
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+        return client.get_result()
 
 # Service logic
 def navigate(x, y, theta):
     print(f"navigate: {x}, {y}, {theta}")
+    movebase_client(x, y, theta)
 
 def handle_navigate(req):
     try:
